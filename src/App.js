09eb992e70b084a9e1fd98aa4960aba471ee7d1d
Nishant Boro <nishant4317@gmail.com>
2021-03-11 03:55:50 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import "./styles/style.css";
+import { DailyWeather } from "./components/DailyWeather";
+
+function App() {
+  return (
+    <div className="App">
+      <DailyWeather></DailyWeather>
+    </div>
+  );
+}
+
+export default App;
